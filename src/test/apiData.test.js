37462809f0d9bd8fdba55f985b34e69ea59325d9
Rudra-Patel-code <Rudra-Patel-code>
2025-09-18 17:38:20 -0600

    initial

---
+++ src/test/apiData.test.js
@@ -0,0 +1,13 @@
+//import the function to be tested
+const fetchData = require("../functions/apiData.js");
+
+//Mock this global fetch function
+global.fetch = jest.fn();
+
+//test suite
+describe("fetchData", () => {
+    beforeEach(() => {
+        //clear the object before running the test again.
+        fetch.mockClear();
+    });
+});
