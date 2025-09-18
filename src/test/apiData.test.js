f3b5248a06a629f8bc00628e68afacd3bc650cf6
Rudra-Patel-code <Rudra-Patel-code>
2025-09-18 17:43:20 -0600

    initial

--- src/test/apiData.test.js
+++ src/test/apiData.test.js
@@ -10,4 +10,19 @@ describe("fetchData", () => {
         //clear the object before running the test again.
         fetch.mockClear();
     });
+
+    test("Fetches dat form the API endpoint and returns it as JSON", () => {
+        //AAA pattern
+        // arrange
+        // Mock response
+
+        const mockResponse = {
+            userId: 1,
+            id: 1,
+            title: "delectus aut autem",
+            completed: false,
+        };
+
+        console.log(mockResponse);
+    });
 });
