7deff85cfff3e8b705d98f66bf3c935a59b14723
Rudra-Patel-code <Rudra-Patel-code>
2025-09-25 17:52:44 -0600

    user model added

---
+++ src/functions/userModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// creating a user schema
+
+const userSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  age: { type: Number, min: 7, max: 120 },
+});
+
+const User = mongoose.model("Users", userSchema);
+
+module.exports = { User };
